85db9e4a5fd7d811ea3e1d82711eb3008d16aa2d
y00jin <59616862+y00jin@users.noreply.github.com>
2020-06-22 09:47:45 +0900

    asdfasdfasdfadf

--- forest12/src/main/java/com/btcdd/codeforest/linux/MypageLinux.java
+++ forest12/src/main/java/com/btcdd/codeforest/linux/MypageLinux.java
@@ -15,7 +15,7 @@ public class MypageLinux {
 	public int deleteProblemAllUsers(Long problemNo) {	
 		
 		try {
-			process = Runtime.getRuntime().exec("find . -name 'prob36' -type d | xargs rm -rf");
+			process = Runtime.getRuntime().exec("find /userDirectory/ -name 'prob" + problemNo + "' -type d | xargs rm -rf");
 		}
 		catch (IOException e) {
 			e.printStackTrace();
