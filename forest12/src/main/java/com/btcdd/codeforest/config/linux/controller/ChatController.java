856db603898be1c711c715a0d26d09885cf76645
박관우 <pgw9470@naver.com>
2020-06-25 10:11:19 +0900

    d

--- forest12/src/main/java/com/btcdd/codeforest/config/linux/controller/ChatController.java
+++ forest12/src/main/java/com/btcdd/codeforest/config/linux/controller/ChatController.java
@@ -60,12 +60,12 @@ public class ChatController {
 		String code = (String) obj.get("code");
 		try {
 			if(pandan) {
-//				process = Runtime.getRuntime().exec("cmd");
+				process = Runtime.getRuntime().exec("cmd");
 				if("c".equals(language)) {
 					RunC rc = new RunC();
 					rc.createFileAsSource(code);
 					errorResult = rc.execCompile();
-					
+					process = Runtime.getRuntime().exec("./test.exe");
 				} else if("cpp".equals(language)) {
 					RunCpp rcpp = new RunCpp();
 					rcpp.createFileAsSource(code);
@@ -111,8 +111,8 @@ public class ChatController {
 			// 에러 stream을 BufferedReader로 받아서 에러가 발생할 경우 console 화면에 출력시킨다.
 			Executors.newCachedThreadPool().submit(() -> {
 				try {
-//					BufferedReader reader = new BufferedReader(new InputStreamReader(stderr, "euc-kr"));
-					BufferedReader reader = new BufferedReader(new InputStreamReader(stderr, "utf-8"));
+					BufferedReader reader = new BufferedReader(new InputStreamReader(stderr, "euc-kr"));
+//					BufferedReader reader = new BufferedReader(new InputStreamReader(stderr, "utf-8"));
 					int c = 0;
 					while ((c = reader.read()) != -1) {
 						char line = (char) c;
@@ -154,8 +154,8 @@ public class ChatController {
 				Executors.newCachedThreadPool().submit(() -> {
 					try {
 //								BufferedReader reader = new BufferedReader(new InputStreamReader(stdout, "euc-kr"));
-								InputStreamReader is = new InputStreamReader(stdout, "utf-8");
-//						InputStreamReader is = new InputStreamReader(stdout, "euc-kr");
+//								InputStreamReader is = new InputStreamReader(stdout, "utf-8");
+						InputStreamReader is = new InputStreamReader(stdout, "euc-kr");
 						
 //								BufferedReader reader = new BufferedReader(new InputStreamReader(stdout, "utf-8"));
 						int c = 0;
@@ -179,7 +179,6 @@ public class ChatController {
 
 		try {
 			Thread.sleep(1000);
-			process = Runtime.getRuntime().exec("./test.exe");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
