d90dca33ab3690042c9668a5cb85d938c4cf2865
taesung <ktsung94@naver.com>
2020-06-25 20:42:30 +0900

    aaa

--- forest12/src/main/java/com/btcdd/codeforest/controller/api/CodeTreeController.java
+++ forest12/src/main/java/com/btcdd/codeforest/controller/api/CodeTreeController.java
@@ -231,8 +231,8 @@ public class CodeTreeController {
  		
 		Map<String, Object> map = new HashMap<>();
 		
-		String[] examOutputSplit = examOutput.split("\\n");
-		String[] compileResult1Split =compileResult1.split("\\n");
+		String[] examOutputSplit = examOutput.split("\n");
+		String[] compileResult1Split =compileResult1.split("\n");
 		
 		boolean[] check =  new boolean[examOutputSplit.length];
 		
