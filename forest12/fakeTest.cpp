76229ef86b4ba3d4793ac348ada9e0fdd2f052c9
박관우 <pgw9470@naver.com>
2020-06-25 17:01:29 +0900

    d

---
+++ forest12/fakeTest.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+	
+    int i;
+	printf("Hello CodeForest!\n");
+
+  	
+    int n = 0;
+fflush(stdout);
+	scanf("%d", &n);
+
+    
+    printf("123123하하하하%d\n", n);
+
+	return 0;
+}
