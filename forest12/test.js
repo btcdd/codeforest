1b60164ddd4b657536836962e310e20489b65765
박관우 <pgw9470@naver.com>
2020-06-29 19:31:55 +0900

    파일 복구

---
+++ forest12/test.js
@@ -0,0 +1,15 @@
+// Run by Node.js
+
+console.log('asdf');
+const readline = require("readline");
+const rl = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout
+});
+
+rl.on("line", function(line) {
+	console.log("Hello Goorm! Your input is", line);
+	rl.close();
+}).on("close", function() {
+	process.exit();
+});
