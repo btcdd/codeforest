685fdae673c426c0596ae53c2560951e2e31c6a5
박관우 <pgw9470@naver.com>
2020-06-30 14:15:51 +0900

    ff

--- forest12/src/main/java/com/btcdd/codeforest/config/linux/controller/CodeTreeChatController.java
+++ forest12/src/main/java/com/btcdd/codeforest/config/linux/controller/CodeTreeChatController.java
@@ -119,11 +119,14 @@ public class CodeTreeChatController {
 					BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(stdin));
 					String input = chatMessage.getContent();
 					// 지술이형 코드!!
-					if(input == null) {
+					if(input == null && submitPandan) {
 						writer.write(codetreeService.getExamInput(subProblemNo) + "\n");
 						writer.flush();
 						readBuffer.setLength(0);
+					} else if(input == null) {
+						return;
 					}
+					
 					if (!("".equals(input)) || input != null) {
 						try {
 							input += "\n";
