c3bdf3af666be2496b7592feca3786428e81a3f9
y00jin <59616862+y00jin@users.noreply.github.com>
2020-06-30 17:38:00 +0900

    asdf

--- forest12/src/main/java/com/btcdd/codeforest/config/linux/controller/CodeTreeChatController.java
+++ forest12/src/main/java/com/btcdd/codeforest/config/linux/controller/CodeTreeChatController.java
@@ -123,11 +123,11 @@ public class CodeTreeChatController {
 					BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(stdin));
 					String input = chatMessage.getContent();
 					
-//					if(submitPandan == true && input == null) {
-//						writer.write(codetreeService.getExamInput(subProblemNo) + "\n");
-//						writer.flush();
-//						readBuffer.setLength(0);
-//					} 
+					if(submitPandan == true && input == null) {
+						writer.write(codetreeService.getExamInput(subProblemNo) + "\n");
+						writer.flush();
+						readBuffer.setLength(0);
+					} 
 //					// 지술이형 코드!!
 //					else 
 					
