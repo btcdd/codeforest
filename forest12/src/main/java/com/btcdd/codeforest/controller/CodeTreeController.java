1f1ca0a54a432ecc30e070c97849cc74c75f0d8b
y00jin <59616862+y00jin@users.noreply.github.com>
2020-07-02 09:21:44 +0900

    url 수정

--- forest12/src/main/java/com/btcdd/codeforest/controller/CodeTreeController.java
+++ forest12/src/main/java/com/btcdd/codeforest/controller/CodeTreeController.java
@@ -40,7 +40,7 @@ public class CodeTreeController {
 	}
 	
 	@Auth
-	@RequestMapping("/codemirror/{saveNo}")
+	@RequestMapping("/{saveNo}")
 	public String mirror(@PathVariable("saveNo") Long saveNo, Model model, HttpSession session) {
 		SaveVo saveVo = codeTreeService.findSaveVo(saveNo);
 		List<SavePathVo> savePathList = codeTreeService.findSavePathList(saveVo.getNo());
@@ -64,40 +64,4 @@ public class CodeTreeController {
 		return "codetree/codetree";
 	}
 }
-/*
- * @RequestMapping(value="", method=RequestMethod.GET) public String mypage() {
- * return "codetree/codetree"; }
- */
-
-
-
-
-/*
- @PostMapping("/list/{userEmail}") //code tree Home화면 띄울때 경로(리스트출력위해 필요)
-public JsonResult codeTreeList(@PathVariable("userEmail") String userEmail) {
-	Map<String, Object> map = new HashMap<>();
-	
-	List<SaveVo> saveVoList = trainingService.selectSaveNoList(_authUser.getNo());
-	
-	List<Long> copy = new ArrayList<>();
-	for(SaveVo vo : saveVoList) {
-		copy.add(vo.getProblemNo());
-	}
-	List<Long> problemNoArray = new ArrayList<>();
-	for(Long no : copy) {
-		if(!problemNoArray.contains(no)) {
-			problemNoArray.add(no);
-		}
-	}
-	//List<SubProblemVo> subProblemList = new ArrayList<>();
-	Map<Long, Object> subProblemList = new HashMap<>();
-	for(Long no : problemNoArray) {
-		subProblemList.put(no, mypageService.findSubProblem(no));
-	}
-	System.out.println(subProblemList);
-	map.put("subProblemList", subProblemList);
-	map.put("saveVoList", saveVoList);
-	return JsonResult.success(map);
-} 
- * */
  
