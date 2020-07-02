50332cbf752ba8c529bcd5a43f6eb799c3f64886
박관우 <pgw9470@naver.com>
2020-07-02 10:40:51 +0900

    d

--- forest12/src/main/java/com/btcdd/codeforest/config/linux/controller/ChatController.java
+++ forest12/src/main/java/com/btcdd/codeforest/config/linux/controller/ChatController.java
@@ -25,7 +25,6 @@ import com.btcdd.codeforest.runlanguage.RunCs;
 import com.btcdd.codeforest.runlanguage.RunJava;
 import com.btcdd.codeforest.runlanguage.RunJs;
 import com.btcdd.codeforest.runlanguage.RunPy;
-import com.btcdd.codeforest.service.CodeTreeService;
 
 @Controller
 public class ChatController {
@@ -37,6 +36,9 @@ public class ChatController {
 	@MessageMapping("/chat")
 	@SendTo("/topic/public")
 	public ChatMessage addUser(String data, @Payload ChatMessage chatMessage, SimpMessageHeaderAccessor headerAccessor) {
+		
+		chatMessage.setErrorPandan(false);
+		
 		String errorResult = "";
 		Boolean pandan = false;
 		
@@ -101,6 +103,8 @@ public class ChatController {
 			OutputStream stdin = process.getOutputStream();
 			InputStream stderr = process.getErrorStream();
 			InputStream stdout = process.getInputStream();
+			
+			StringBuffer readBuffer2 = new StringBuffer();
 
 			// 에러 stream을 BufferedReader로 받아서 에러가 발생할 경우 console 화면에 출력시킨다.
 			Executors.newCachedThreadPool().submit(() -> {
@@ -109,6 +113,7 @@ public class ChatController {
 					int c = 0;
 					while ((c = reader.read()) != -1) {
 						char line = (char) c;
+						readBuffer2.append(line);
 					}
 				} catch (IOException e) {
 					e.printStackTrace();
@@ -129,6 +134,7 @@ public class ChatController {
 					if (!("".equals(input)) || input != null) {
 						try {
 							input += "\n";
+							readBuffer2.append(input);
 							writer.write(input);
 							writer.flush();
 							readBuffer.setLength(0);
